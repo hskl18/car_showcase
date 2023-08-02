f67d14c8e71eaaf4a8d3e27762ff789965f14a25
hanbintan2003 <thb182003@gmail.com>
2023-08-02 02:10:02 -0700

    stop

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,25 @@ export default function Home() {
   return (
     <main className="overflow-hidden ">
       <Hero />
+      
+      <div className='mt-12 padding-x padding-y max-width' id='discover'>
+        <div className='home__text-container'>
+          <h1 className='text-4xl font-extrabold'>
+            Car Catelogue
+          </h1>
+          <p>
+            Explore the car catelogue and find the car that suits you best.
+          </p>
+        </div>
+        <div className='home__filters'>
+          <SeachBar />
+
+          <div className='home__filter-container'>
+            <CustomFilter title ="fuel" />
+            <CustomFilter title ="year" />
+          </div>
+        </div>
+      </div>
     </main>
   )
 }
