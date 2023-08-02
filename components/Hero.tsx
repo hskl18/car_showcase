ad1698e5b6b1f84ff0d6aef4b8866c317f334e44
hanbintan2003 <thb182003@gmail.com>
2023-08-01 22:52:01 -0700

    hero section semi done

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,9 +1,15 @@
+"use client"
+
 import React from 'react'
 import Image from 'next/image'
 import { CustomButton } from '.'
 
 
 const Hero = () => {
+  const handleScroll = () => {
+
+  }
+
   return (
     <div className='hero'>
       <div className='flex-1 pt-36 padding-x'>
@@ -13,7 +19,18 @@ const Hero = () => {
         <p className='hero__subtitle'>
           Steamline Your Car Rental Experience With Our Simple Search Engine.
         </p>
-        <CustomButton />
+        <CustomButton 
+          title = "Explore Cars"
+          containerStyles = "bg-primary-blue text-white rounded-full mt-10"
+          handleClick = {handleScroll}
+        />
+      </div>
+
+      <div className='hero__image-container '>
+        <div className='hero__image'>
+          <Image src ="/hero.png" alt='hero' fill className='object-contain'/>
+          <div className='hero__image-overlay '></div>
+        </div>
       </div>
     </div>
   )
