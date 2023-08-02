4257ef3ab23b19d426a1a5bbcfe00a5a485086a0
hanbintan2003 <thb182003@gmail.com>
2023-08-02 01:59:35 -0700

    finish the nav and footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,9 @@
 import Link from "next/link"
 import Image from "next/image"
 
+import { footerLinks } from "@/constants"
+import { it } from "node:test"
+
 const Footer = () => {
   return (
     <footer className="flex flex-col text-black-100 mt-5 border-t border-gray-100">
@@ -14,10 +17,36 @@ const Footer = () => {
           </div>
 
           <div className = "footer__links">
-            
-          </div>
+            {footerLinks.map((link) => (
+              <div key = {link.title} className="footer__link">
+                <h3 className="font-bold">
+                  {link.title}
+                </h3>
 
+                {link.links.map((item) => (
+                  <Link key={item.title} href={item.url} className="text-gray-500">
+                    {item.title}
+                  </Link>
+                ))}
+                
+              </div>
+            ))}
+          </div>
         </div>
+
+        <div className='flex justify-between items-center flex-wrap mt-10 border-t border-gray-100 sm:px-16 px-6 py-10'>
+          <p>@2023 CarHub. All rights reserved</p>
+
+          <div className="footer__copyrights-link">
+            <Link href="/" className="text-gray-500">
+              Privacy & Policy
+            </Link>
+            <Link href="/" className="text-gray-500">
+              Terms & Condition
+            </Link>
+          </div>
+        </div>
+
     </footer>
   )
 }
