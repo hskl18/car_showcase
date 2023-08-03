5cb01d70b56d53d92b786b6ea5ca52dd63ae0452
hanbintan2003 <thb182003@gmail.com>
2023-08-03 00:20:10 -0700

    finish filter

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,15 +1,21 @@
 "use client"
 
+import { useState } from "react"
 import { SearchManufacturer } from "."
 
 const SearchBar = () => {
+  const [Manufacturer, setManufacturer] = useState("")
+
   const handleSearch = ()=>{
 
   }
   return (
     <form className="searchbar" onSubmit={handleSearch}>
       <div className="searchbar__item">
-
+        <SearchManufacturer 
+          Manufacturer={Manufacturer}
+          setManufacturer={setManufacturer}
+        />
       </div>
     </form>
   )
--- types/index.ts
+++ types/index.ts
@@ -1,4 +1,4 @@
-import { MouseEventHandler } from "react"
+import React, { MouseEventHandler } from "react"
 
 export interface CustomButtonProps {
     title: string
@@ -6,6 +6,7 @@ export interface CustomButtonProps {
     handleClick?: MouseEventHandler<HTMLButtonElement>
     btnType?: "button" | "submit" 
 }
-export interface CustomFilterProps {
-    title: string
+export interface SearchManufacturerProps {
+    manufacturer: string
+    setManufacturer: (manufacturer: string) => void
 }
