0bb3a7b211e964537540a16206134da0f2498fe7
hanbintan2003 <thb182003@gmail.com>
2023-08-06 23:01:13 -0700

    cool

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import { Hero, SearchBar, CustomFilter, CarCard } from '@/components'
+import { fuels, yearsOfProduction } from '@/constants'
 import { HomeProps } from '@/types'
 import { fetchCars } from '@/utils'
 import Image from 'next/image'
@@ -34,8 +35,8 @@ export default async function Home({ searchParams }: HomeProps) {
           <SearchBar />
 
           <div className='home__filter-container'>
-            <CustomFilter title ="fuel" />
-            <CustomFilter title ="year" />
+            <CustomFilter title ="fuel" options = {fuels}/>
+            <CustomFilter title ="year" options = {yearsOfProduction}/>
           </div>
         </div>
 
